796b5472d310efae2467f022e0136ed1eb4823d4
myokoaung002 <myokoaung002@gmail.com>
2024-12-04 21:20:53 +0630

    commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+const prisma = require("./prismaClient");
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const { contentRouter } = require("./routers/content");
+app.use("/content", contentRouter);
+
+const { userRouter } = require("./routers/user");
+app.use("/", userRouter);
+
+app.get("/info", (req, res) => {
+    res.json({ msg: "Yay cha api" });
+});
+
+
+const server = app.listen(8000, () => {
+    console.log("Yaycha API start at 8000...");
+});
+
+const gracefulShutdown = async () => {
+    await prisma.$disconnect();
+    server.close(() => {
+      console.log("Yaycha API closed.");
+      process.exit(0);
+    });
+   };
+  
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);
